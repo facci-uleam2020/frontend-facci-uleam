c3f580a7c928b748afa2fa874b7b755437b1cca3
eder <ederzambranomero@gmail.com>
2021-05-30 20:50:49 -0500

    first commit

---
+++ src/api/config.js
@@ -0,0 +1,10 @@
+// export const basePath = "https://cursosonlinefacci.herokuapp.com/api";
+
+export const basePath = "http://localhost:2002/api";
+
+// export const basePath = "https://cursosfaccionline.azurewebsites.net/api";
+// export const basePath = "https://16c0d608905d.ngrok.io/api";
+
+// http://8eb5eefceb69.ngrok.io
+
+export const apiVersion = "v1";
