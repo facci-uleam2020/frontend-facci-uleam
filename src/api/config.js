56455bc86cea17f91bc1105465c7cf159f084833
facci-uleam2020 <84675845+facci-uleam2020@users.noreply.github.com>
2021-06-01 13:06:14 +0000

    Update config.js

--- src/api/config.js
+++ src/api/config.js
@@ -3,7 +3,7 @@
 // export const basePath = "http://localhost:2002/api";
 
 // export const basePath = "https://cursosfaccionline.azurewebsites.net/api";
- export const basePath = " https://f4c4d7304c4b.ngrok.io/api";
+ export const basePath = "https://4ac586262a53.ngrok.io/api";
 //export const basePath = "http://52.22.107.63:2002/api";
 
 // http://8eb5eefceb69.ngrok.io
